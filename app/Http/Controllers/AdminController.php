fff6625ad08034e838ba2909310992645485f12e
salomeMumladze <salomemumladze775@gmail.com>
2023-11-07 18:15:19 +0400

    fixed dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Order;
+use App\Models\Client;
 
 class AdminController extends Controller
 {
     public function dashboard(){
-        return view('admin.dashboard');
+        $orders = Order::All();
+        $clients = Client::All();
+        return view('admin.dashboard')->with('orders', $orders)->with('clients', $clients);
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -28,9 +28,8 @@
             <!-- small box -->
             <div class="small-box bg-info">
               <div class="inner">
-                <h3>150</h3>
-
-                <p>New Orders</p>
+                <h3>{{count($orders)}}</h3>
+                <p>Orders</p>
               </div>
               <div class="icon">
                 <i class="ion ion-bag"></i>
@@ -58,8 +57,7 @@
             <!-- small box -->
             <div class="small-box bg-warning">
               <div class="inner">
-                <h3>44</h3>
-
+                <h3>{{count($clients)}}</h3>
                 <p>User Registrations</p>
               </div>
               <div class="icon">
